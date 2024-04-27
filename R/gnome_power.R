dc24c185e45181231e703a2fb1a742f08f206aa2
josefinabernardo <113699978+josefinabernardo@users.noreply.github.com>
2024-04-27 10:55:28 +0200

    Minor edits

--- R/gnome_power.R
+++ R/gnome_power.R
@@ -11,7 +11,6 @@
 #' @examples
 #' gnome_power(df = 1, ncp = 10)
 #' gnome_power(alpha = .01, df = 1, ncp = 5)
-
 gnome_power <- function(alpha = .05, df, ncp) {
   critical_chi2 <- qchisq(alpha, df, lower.tail = F)
   if (abs(ncp) < .0001) { ncp = 0 }
